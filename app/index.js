ec156363b7c9bc36b3ccc54cacb984301c0902e6
Alexander Zverev <zverev@Adm-Scanexs-iMac.local>
2016-01-22 21:04:53 +0300

    add --name option

--- app/index.js
+++ app/index.js
@@ -1,27 +1,41 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-    createFiles: function() {
-        var done = this.async();
-        this.prompt({
-            type: 'input',
-            name: 'name',
-            message: 'Your project name',
-            default: '' // Default to current folder name
-        }, function(answers) {
-            var widgetName = answers.name;
-            var scriptFileName = (answers.name || 'script') + '.js';
-            var stylesFileName = (answers.name || 'styles') + '.css';
-            
-            this.fs.copyTpl(this.templatePath('index.html'), this.destinationPath('index.html'), {
-                scriptFileName: scriptFileName,
-                stylesFileName: stylesFileName,
-                projectName: answers.name || 'The app'
-            });
-            this.fs.copy(this.templatePath('script.js'), this.destinationPath(scriptFileName));
-            this.fs.copy(this.templatePath('styles.css'), this.destinationPath(stylesFileName));
+    constructor: function() {
+        generators.Base.apply(this, arguments);
+        this.option('name');
+    },
+    prompting: {
+        createFiles: function() {
+            if (this.options.name) {
+                doCreateFiles.call(this, this.options.name);
+                return;
+            }
 
-            done();
-        }.bind(this));
+            var done = this.async();
+            this.prompt({
+                type: 'input',
+                name: 'name',
+                message: 'Your project name',
+                default: '' // Default to current folder name
+            }, function(answers) {
+                doCreateFiles.call(this, answers.name);
+                done();
+            }.bind(this));
+
+            function doCreateFiles(name) {
+                var widgetName = name;
+                var scriptFileName = (name || 'script') + '.js';
+                var stylesFileName = (name || 'styles') + '.css';
+
+                this.fs.copyTpl(this.templatePath('index.html'), this.destinationPath('index.html'), {
+                    scriptFileName: scriptFileName,
+                    stylesFileName: stylesFileName,
+                    projectName: name || 'The app'
+                });
+                this.fs.copy(this.templatePath('script.js'), this.destinationPath(scriptFileName));
+                this.fs.copy(this.templatePath('styles.css'), this.destinationPath(stylesFileName));
+            }
+        }
     }
 });
